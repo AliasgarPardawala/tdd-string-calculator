c0164cb8a713f328dbf5e7b47e4bea53f3b0eb17
Aliasgar <aliasgar.pardawala@noovosoft.com>
2024-11-06 20:11:25 +0530

    Add test case for single number input

--- src/calulator.test.ts
+++ src/calulator.test.ts
@@ -4,4 +4,10 @@ describe('Calculator', () => {
     it('should return 0 for an empty string', () => {
         expect(add('')).toBe(0);
     });
+
+    it('should return the number when a single number is given', () => {
+        expect(add("1")).toBe(1);
+        expect(add("3")).toBe(3);
+        expect(add("5")).toBe(5);
+    });
 })
