e237b06400d405690fc24ec49fcbb34fa7740965
Aliasgar <aliasgar.pardawala@noovosoft.com>
2024-11-06 20:12:17 +0530

    Add test case for multiple comma separated numbers

--- src/calulator.test.ts
+++ src/calulator.test.ts
@@ -10,4 +10,10 @@ describe('Calculator', () => {
         expect(add("3")).toBe(3);
         expect(add("5")).toBe(5);
     });
+
+    it('should return the sum of multiple numbers separated by commas', () => {
+        expect(add("1,2")).toBe(3);
+        expect(add("1,2,3")).toBe(6);
+        expect(add("5,10,15")).toBe(30);
+    });
 })
